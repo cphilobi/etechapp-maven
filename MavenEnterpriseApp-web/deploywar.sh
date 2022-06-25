5b1a01333e27aa91e04628f6be7d91bc89d96929
cphilobi <mychidany@gmail.com>
2022-06-25 05:21:53 +0000

    Update script full path instead of using environmental variables

--- MavenEnterpriseApp-web/deploywar.sh
+++ MavenEnterpriseApp-web/deploywar.sh
@@ -2,6 +2,6 @@
 
 # This script will automate the deployment of the .war package from the Jenkins server to the tomcat server over ssh.
 # This script will be run from jenkins as a step in the deploy stage while calling the Jenkinsfile from GitHub.
-# This script also makes use of the $HOME environmental variable
 
-scp -i $HOME/mavenrepo/etechapp-maven/MavenEnterpriseApp-web/key $HOME/mavenrepo/etechapp-maven/MavenEnterpriseApp-web/target/MavenEnterpriseApplication.war ubuntu@172.31.21.31:/opt/tomcat/apache-tomcat-9.0.64/webapps
+
+scp -i /home/ubuntu/mavenrepo/etechapp-maven/MavenEnterpriseApp-web/key /home/ubuntu/mavenrepo/etechapp-maven/MavenEnterpriseApp-web/target/MavenEnterpriseApplication.war ubuntu@172.31.21.31:/opt/tomcat/apache-tomcat-9.0.64/webapps
